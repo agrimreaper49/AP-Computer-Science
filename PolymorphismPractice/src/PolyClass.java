0263e46a3f4a0e2a306e56d44e45dbb450ca1248
agrimreaper49 <75flyershd@gmail.com>
2020-02-13 11:47:05 -0500

    New polymorphism project

---
+++ PolymorphismPractice/src/PolyClass.java
@@ -0,0 +1,30 @@
+public class PolyClass {
+    public static void main (String [] args){
+        Car newCar =  new Toyota("Tacoma", 2020);
+        ((Toyota)newCar).drive(50);
+    }
+}
+class Car {
+    public void drive(){
+        System.out.println("Drive method of Car");
+    }
+
+}
+class Toyota extends Car{
+    private String name = "";
+    private int year = 0;
+    private boolean isDriving;
+
+    public Toyota(String name, int year){
+        this.name = name;
+        this.year = year;
+    }
+
+    public void drive(int n){
+        isDriving = true;
+        for ( int i = 0; i < n; i++ ){
+            i++;
+        }
+        System.out.println("Drive Complete, drove for " + n + " miles");
+    }
+}
